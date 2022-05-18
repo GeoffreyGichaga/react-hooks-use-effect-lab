46a74dcef26c58b7659d2c717feac12e0241359c
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-05-18 12:57:26 +0300

    completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(()=>{
+   const quizTime =  setTimeout(()=>{
+      setTimeRemaining(timeRemaining -1)
+    },1000)
+
+    return function cleanUp(){
+      if(timeRemaining===0)
+      {
+        clearInterval(quizTime)
+        setTimeRemaining(10)
+        onAnswered(false)
+
+      }
+    }
+
+    
+  },[timeRemaining,onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
